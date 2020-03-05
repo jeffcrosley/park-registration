b121f706cc793357a60f3ea1b0765e3c2bb3ee1a
Jake Casassa <jakecasassa@gmail.com>
2020-03-05 17:28:11 -0500

    populated parks

--- src/main/java/com/techelevator/parks/model/ParkDAO.java
+++ src/main/java/com/techelevator/parks/model/ParkDAO.java
@@ -1,5 +1,10 @@
 package com.techelevator.parks.model;
 
+import java.util.List;
+
 public interface ParkDAO {
 
-}
+		public List<Park> getAllParks();
+	}
+
+
--- src/main/java/com/techelevator/parks/model/jdbc/JDBCParkDAO.java
+++ src/main/java/com/techelevator/parks/model/jdbc/JDBCParkDAO.java
@@ -1,9 +1,15 @@
 package com.techelevator.parks.model.jdbc;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.techelevator.parks.model.Park;
 import com.techelevator.parks.model.ParkDAO;
 
 public class JDBCParkDAO implements ParkDAO {
@@ -14,4 +20,27 @@ private JdbcTemplate jdbcTemplate;
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-}
+	@Override
+	public List<Park> getAllParks() {
+		String allParks = "SELECT * FROM park ";
+		
+		ArrayList<Park> parks = new ArrayList<Park>();
+		
+		SqlRowSet rowset = jdbcTemplate.queryForRowSet(allParks);
+		
+		while(rowset.next()) {
+			Park p = new Park();
+			p.setId(rowset.getLong("park_id"));
+			p.setName(rowset.getString("name"));
+			p.setLocation(rowset.getString("location"));
+			p.setEstablishedDate(rowset.getDate("establish_date").toLocalDate());
+			p.setArea(rowset.getInt("area"));
+			p.setAnnualVisitors(rowset.getInt("visitors"));
+			p.setDescription(rowset.getString("description"));
+			parks.add(p);			
+		}		
+		return parks;
+	}
+	}
+
+
