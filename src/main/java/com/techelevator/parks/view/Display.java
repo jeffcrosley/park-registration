9c54846944090b6991beee045a721f8ac7788b36
Jake Casassa <jakecasassa@gmail.com>
2020-03-06 10:28:00 -0500

    SysOut added but also sent via river and pasted

--- src/main/java/com/techelevator/parks/view/Display.java
+++ src/main/java/com/techelevator/parks/view/Display.java
@@ -2,17 +2,20 @@ package com.techelevator.parks.view;
 
 import java.util.List;
 
+
 import com.techelevator.parks.model.Park;
 
 public class Display {
 
 	private static String divider = "------------------------------------------------------------";
 	
-	public static void displayAllParks(List<Park> parks) {
-		System.out.println("Select a Park for Further Details");
-		System.out.println(divider);
-		for (Park park : parks) {
-			System.out.println(park.getId() + " " + park.getName());
-		}
+	public static void printParkInfo(Park selectedPark) {
+		System.out.println(selectedPark.getName());
+		System.out.println(selectedPark.getLocation());
+		System.out.println(selectedPark.getEstablishedDate());
+		System.out.println(selectedPark.getArea());
+		System.out.println(selectedPark.getAnnualVisitors());
+		System.out.println(selectedPark.getDescription());
 	}
-}
+	
+} 
