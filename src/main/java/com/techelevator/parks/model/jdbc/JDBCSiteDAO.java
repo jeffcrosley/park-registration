7905295d4508205568bf05029fbed7e0d098b08a
Jeff Crosley <jeffcrosley@gmail.com>
2020-03-07 14:05:46 -0500

    working code for SiteDAO tests

--- src/main/java/com/techelevator/parks/model/jdbc/JDBCSiteDAO.java
+++ src/main/java/com/techelevator/parks/model/jdbc/JDBCSiteDAO.java
@@ -21,45 +21,31 @@ public class JDBCSiteDAO implements SiteDAO {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	static final String searchForReservation = "SELECT * FROM site where " + 
-			"campground_id = '?' AND " + 
-			"site_id NOT IN " + 
-			"(SELECT DISTINCT site.site_id FROM site " + 
-			"JOIN reservation ON site.site_id = reservation.site_id " + 
-			"WHERE " + 
-			"reservation.from_date BETWEEN ? AND ? " + 
-			"OR reservation.to_date BETWEEN ? AND ? " + 
-			"OR ? BETWEEN reservation.from_date AND reservation.to_date " + 
-			"OR ? BETWEEN reservation.from_date AND reservation.to_date) ";
+	static final String sqlFindAvailableSites = "SELECT * FROM site "+ 
+			"WHERE campground_id = ?" +
+			"AND site_id NOT IN " + 
+				"(SELECT DISTINCT site.site_id FROM site " + 
+					"JOIN reservation ON site.site_id = reservation.site_id " + 
+				"WHERE reservation.from_date BETWEEN ? AND ? " + 
+				"OR reservation.to_date BETWEEN ? AND ? " + 
+				"OR ? BETWEEN reservation.from_date AND reservation.to_date " + 
+				"OR ? BETWEEN reservation.from_date AND reservation.to_date)";
+	
 	@Override
-	public List<Site> getAvailableSites(Campground selectedCampground, LocalDate desiredArrivalDate,
-			LocalDate desiredDepartureDate) {
-	String happyPlace = "SELECT * FROM site where " +
-			"campground_id = '?' AND site_id NOT IN " +
-			"(SELECT DISTINCT site.site_id FROM site " +
-			"JOIN reservation ON site.site_id = reservation.site_id " +
-			"WHERE reservation.from_date BETWEEN '?' AND '?') ";
+	public List<Site> getAvailableSites(Campground selectedCampground, LocalDate desiredArrivalDate, LocalDate desiredDepartureDate) {
 		ArrayList<Site> availSites = new ArrayList<Site>();
-		SqlRowSet rowset = jdbcTemplate.queryForRowSet(happyPlace, selectedCampground, desiredArrivalDate, desiredDepartureDate);
+		SqlRowSet rowset = jdbcTemplate.queryForRowSet(sqlFindAvailableSites, selectedCampground.getCampgroundId(), desiredArrivalDate, desiredDepartureDate, desiredArrivalDate, desiredDepartureDate, desiredArrivalDate, desiredDepartureDate);
 		while(rowset.next()) {
-			Site s = new Site();
+			Site s = new Site();				
 			s.setId(rowset.getLong("site_id"));
+			s.setCampgroundId(rowset.getLong("campground_id"));
+			s.setSite_number(rowset.getLong("site_number"));
 			s.setMaxOccupancy(rowset.getLong("max_occupancy"));
 			s.setAccessible(rowset.getBoolean("accessible"));
 			s.setMaxRVLength(rowset.getLong("max_rv_length"));
 			s.setHasUtilities(rowset.getBoolean("utilities"));
-			
+			availSites.add(s);
 		}
 		return availSites;
 	}
-	
-
-	private String convertDateToMonthNumber(LocalDate date) {
-		
-		String output = date.getMonth().toString();
-		
-		return output;
-		
-	}
-
 }
