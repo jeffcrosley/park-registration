be01bd6daf6d2dfa02ceed6898ac66074c005dd6
Jake Casassa <jakecasassa@gmail.com>
2020-03-07 13:15:05 -0500

    trying to get available reservations to work

--- src/main/java/com/techelevator/parks/model/jdbc/JDBCReservationDAO.java
+++ src/main/java/com/techelevator/parks/model/jdbc/JDBCReservationDAO.java
@@ -1,6 +1,6 @@
 package com.techelevator.parks.model.jdbc;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import javax.sql.DataSource;
 
@@ -23,21 +23,10 @@ public class JDBCReservationDAO implements ReservationDAO {
 	}
 	
 	@Override
-	public Reservation createReservation(Site selectedSite, String reservationName, Date arrivalDate,
-			Date departureDate) {
-		String searchForReservation = "SELECT * FROM site where " + 
-				"campground_id = ? AND " + 
-				"site_id NOT IN " + 
-				"(SELECT DISTINCT site.site_id FROM site " + 
-				"JOIN reservation ON site.site_id = reservation.site_id " + 
-				"WHERE " + 
-				"reservation.from_date BETWEEN '?' AND '?' " + 
-				"OR reservation.to_date BETWEEN '2020-03-04' AND '2020-03-08' " + 
-				"OR '2020-03-04' BETWEEN reservation.from_date AND reservation.to_date " + 
-				"OR '2020-03-08' BETWEEN reservation.from_date AND reservation.to_date) ";
-		SqlRowSet rowset = jdbcTemplate.queryForRowSet(searchForReservation);
-		
-		return null;
-	}
+	public Reservation createReservation(Site selectedSite, String reservationName, LocalDate arrivalDate,
+			LocalDate departureDate) {
+				return null;
+	
 
+	}
 }
