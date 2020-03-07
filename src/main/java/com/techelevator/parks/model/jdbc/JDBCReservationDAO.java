a6937fb434269134ef50a68123ad7c0e9f897fac
Jake Casassa <jakecasassa@gmail.com>
2020-03-07 11:08:35 -0500

    added JQL statement for making a reservation

--- src/main/java/com/techelevator/parks/model/jdbc/JDBCReservationDAO.java
+++ src/main/java/com/techelevator/parks/model/jdbc/JDBCReservationDAO.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.parks.model.Reservation;
 import com.techelevator.parks.model.ReservationDAO;
@@ -17,11 +18,25 @@ public class JDBCReservationDAO implements ReservationDAO {
 	public JDBCReservationDAO(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-
+	void createReservation() {
+		
+	}
+	
 	@Override
 	public Reservation createReservation(Site selectedSite, String reservationName, Date arrivalDate,
 			Date departureDate) {
-		// TODO Auto-generated method stub
+		String searchForReservation = "SELECT * FROM site where " + 
+				"campground_id = ? AND " + 
+				"site_id NOT IN " + 
+				"(SELECT DISTINCT site.site_id FROM site " + 
+				"JOIN reservation ON site.site_id = reservation.site_id " + 
+				"WHERE " + 
+				"reservation.from_date BETWEEN '?' AND '?' " + 
+				"OR reservation.to_date BETWEEN '2020-03-04' AND '2020-03-08' " + 
+				"OR '2020-03-04' BETWEEN reservation.from_date AND reservation.to_date " + 
+				"OR '2020-03-08' BETWEEN reservation.from_date AND reservation.to_date) ";
+		SqlRowSet rowset = jdbcTemplate.queryForRowSet(searchForReservation);
+		
 		return null;
 	}
 
